b227357a555e25493955d7928cc77a148b98af06
Franklin Chieze <chieze.franklin@gmail.com>
2019-06-05 19:13:47 +0100

    implement state management with react-redux

--- src/components/Range.js
+++ src/components/Range.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Range as MaterializeRange } from 'react-materialize';
 
-import { Range as MaterializeRange } from 'react-materialize'
+import { changeLightBulbColor } from '../react-redux/actions';
 
 class Range extends Component {
   rangeChanged = (e) => {
-    console.log(this.props.name + ':' + e.target.value)
+    this.props.changeLightBulbColor({
+      [this.props.name]: e.target.value
+    });
   }
   render() {
     return (<MaterializeRange
       min="0"
       max="255"
       name={this.props.name}
+      value={this.props.color[this.props.name]}
       onChange={this.rangeChanged} />)
   }
 }
 
-export default Range;
+const mapDispatchToProps = {
+  changeLightBulbColor
+};
+
+const mapStateToProps = state => ({
+  color: state.color
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Range);
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Switch as MaterializeSwitch } from 'react-materialize'
+import { Switch as MaterializeSwitch } from 'react-materialize';
+
+import { changeLightBulbState } from '../react-redux/actions';
 
 class Switch extends Component {
   switchChanged = (e) => {
-    console.log(e.target.checked)
+    this.props.changeLightBulbState(e.target.checked);
   }
   render() {
     return (<MaterializeSwitch
       offLabel="Off"
       onLabel="On"
+      checked={this.props.on}
       onChange={this.switchChanged} />)
   }
 }
 
 const mapDispatchToProps = {
-  deleteTodo,
-  toggleTodo
+  changeLightBulbState
 };
 
 const mapStateToProps = state => ({
-  todos: state.todos.items
+  on: state.state.on
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Switch);
